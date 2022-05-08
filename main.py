4c799f911f58f8608d57776bc1a1fccd7fb3c353
waylake <hwangdoyeonyu@gmail.com>
2022-05-09 07:39:37 +0900

    delete the code that not seems like base model

--- main.py
+++ main.py
@@ -2,10 +2,3 @@ from typing import List
 from utils.utils import Utils
 
 utils = Utils()
-
-temp: List = [['aa', 'bbbb', 'dddddd'],
-              ['a', 'dddddddddd', 'iiiiiii'],
-              ['dddddddddddddd', 'dd', 'ddddddddddddddddd']]
-
-elements_max_length = utils.max_length_check(rows=temp)
-print(elements_max_length)

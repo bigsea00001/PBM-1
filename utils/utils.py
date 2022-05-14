fc52c1b5edff19f7360b2702994db16e714f65e5
waylake <hwangdoyeonyu@gmail.com>
2022-05-15 01:18:13 +0900

    add dir check function in Utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,27 @@
+"""
+python base model's utils
+"""
+import os
+from typing import List
+
+from .info import base
+
+
 class Utils:
+    """
+    Utils for main
+    """
+
     def __init__(self):
         pass
+
+    @classmethod
+    def _dir_check(cls) -> None:
+        """
+        check the directories for Util
+        """
+        directories: List = base.base_info['directories']
+
+        for directory in directories:
+            if not os.path.exists(directory):
+                os.mkdir(directory)

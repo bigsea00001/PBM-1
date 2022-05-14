b5da24105bab67c824452fc728b275cd5b82c16f
waylake <hwangdoyeonyu@gmail.com>
2022-05-15 02:14:26 +0900

    add DB model in Util

---
+++ utils/db_models/db_model.py
@@ -0,0 +1,8 @@
+class DB_model:
+    def __init__(self, host, db_name, user, password, port):
+        self.host = host
+        self.db_name = db_name
+        self.user = user
+        self.password = password
+        self.port = port
+

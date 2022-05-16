f19e7b9ae2840939895086ba8ad41b0545dd3e5f
waylake <hwangdoyeonyu@gmail.com>
2022-05-16 23:06:47 +0900

    fix and add some codes

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from typing import List
 from utils.utils import Utils
 
 utils = Utils()
+
+test_row = [['dd','ddd', 'eee'], ['a', '333333', 'what ss']]
+max_len = utils.DB_handler.get_max_len(rows=test_row)
+print(max_len)

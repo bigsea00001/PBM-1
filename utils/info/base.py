8b3fb89bbd304b8cb8a73d28c40f1ccfdc2812b9
waylake <hwangdoyeonyu@gmail.com>
2022-05-19 19:57:53 +0900

    edit info file

--- utils/info/base.py
+++ utils/info/base.py
@@ -1,13 +1,4 @@
 base_info = {
-    'selenium': {
-        'ops': {
-            'headless',
-            'ignore-certificate-errors',
-            'dns-prefetch-disable',
-            'disable-extensions',
-        },
-        'prefs': {'download.default_directory' : 'Data/chrome_driver'},
-    },
     'directories': [
         'Data',
         'Data/pkl',
@@ -15,9 +6,14 @@ base_info = {
         'Data/download',
         'log',
     ],
-    'functions': [
-        'a_function',
-        'b_function',
-        'c_function',
+}
+
+selenium_info = {
+    'options': [
+        'ignore-certificate-errors',
+        'dns-prefetch-disable',
+        'disable-extensions',
+        'headless',
     ],
+    'prefs': {'download.default_directory' : 'Data/chrome_driver'},
 }

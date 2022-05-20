5cc1e387f8c16ad1be806fb2c54a9f0ec4a14421
waylake <hwangdoyeonyu@gmail.com>
2022-05-20 15:35:40 +0900

    add parameter check case

--- utils/utils.py
+++ utils/utils.py
@@ -6,8 +6,7 @@ import pymysql
 from typing import List, Dict
 
 from .info import base
-from .models.database import DB_model, DB_handler
-from .models.pre_set import Pre_set as PS
+from .models import CustomLogger, Pre_set, DB_handler, DB_model
 
 
 class Utils:
@@ -18,7 +17,9 @@ class Utils:
     def __init__(self, **kwargs):
         self.kwargs = kwargs
         self.base_info = base.base_info
-        self.ps = PS(info=base)
+        self.pre_set= Pre_set(info=base)
+        self.logger = CustomLogger().config()
+        self.db_handler = DB_handler()
         self.value: Dict = {}
         self.parameter_check()
 
@@ -31,12 +32,17 @@ class Utils:
         for param in selected_params:
             if (kwargs_info[param]['type'] == 'func' and
                     self.kwargs[param] is True):
-                if kwargs_info[param]['return'] is False:
-                    func = kwargs_info[param]['name']
-                    eval(f'self.ps.{func}()')
+                if kwargs_info[param]['_return'] is None:
+                    func = kwargs_info[param]['func']
+                    eval(f'self.pre_set.{func}()')
 
-                elif kwargs_info[param]['return'] is True:
-                    func = kwargs_info[param]['name']
-                    _return_name = kwargs_info[param]['return_name']
-                    exec(f'{_return_name} = self.ps.{func}()')
+                elif not kwargs_info[param]['_return'] is None:
+                    func = kwargs_info[param]['func']
+                    _return_name = kwargs_info[param]['_return']
+                    exec(f'{_return_name} = self.pre_set.{func}()')
                     self.value[_return_name] = eval(_return_name)
+
+            elif kwargs_info[param]['type'] == 'DB_model':
+                func = kwargs_info[param]['func']
+                db_model = self.kwargs[param]
+                eval(f'self.pre_set.{func}(db_model)')

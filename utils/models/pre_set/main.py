9f36688d7846772629688e8edea75078089b4235
waylake <hwangdoyeonyu@gmail.com>
2022-05-20 15:28:25 +0900

    add create_db_model function

--- utils/models/pre_set/main.py
+++ utils/models/pre_set/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Dict
+from typing import List, Dict, Type
 
 
 class Pre_set:
@@ -26,3 +26,24 @@ class Pre_set:
         options.add_experimental_option('prefs', pre_experi_options)
 
         return options
+
+
+    def _create_db_model(self, db_model):
+        """
+        create_db_model and save to json
+        """
+        import json
+
+        model = db_model
+
+        model_json= {
+            'host' : model.host,
+            'db_name': model.db_name,
+            'user': model.user,
+            'password': model.password,
+            'port': model.port
+        }
+
+        with open('Data/model/Database.json', 'w') as json_file:
+            json.dump(model_json, json_file, indent=6)
+            json_file.close()

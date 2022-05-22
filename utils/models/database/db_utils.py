863e8d6c9dbddac87230e38ea4d987b7a4ee4350
waylake <hwangdoyeonyu@gmail.com>
2022-05-22 14:49:50 +0900

    edit code

--- utils/models/database/db_utils.py
+++ utils/models/database/db_utils.py
@@ -11,22 +11,6 @@ class DB_model:
         self.password = password
         self.port = port
 
-    def get_model(self, _db_name):
-        file_path = 'Data/model/database_models.json'
-        with open(file_path, 'r', encoding='utf-8') as json_file:
-            json_data = json.load(json_file)
-
-        #  db_name = json_data[self.get_name]['db_name']
-        db_name = _db_name
-        host = json_data[db_name]['host']
-        user = json_data[db_name]['user']
-        password = json_data[db_name]['password']
-        port = json_data[db_name]['port']
-        _model = DB_model(host=host, db_name=db_name,
-                          user=user, password=password, port=port)
-
-        return _model
-
 
 class DB_handler:
     def __init__(self):
@@ -50,21 +34,7 @@ class DB_handler:
 
         return _return_row
 
-    @classmethod
-    def get_conn(cls, db_name: str = None) -> Union[None, pymysql.connect]:
-        if db_name is None:
-            return None
-
-        model = DB_model().get_model(_db_name=db_name)
-
-        conn = pymysql.connect(host=model.host,
-                               db=model.db_name,
-                               user=model.user,
-                               password=model.password,
-                               port=model.port)
-        return conn
-
-    def insert_db(self, input_rows, table_info):
+    def insert_db(self, input_rows, table_info, conn):
         table_name = table_info['table_name']
         table_scheme = table_info['scheme']
         scheme_format = tuple(table_scheme)
@@ -72,7 +42,6 @@ class DB_handler:
         values_format = tuple(values_format)
 
         try:
-            conn = self.get_conn(model=None)  # define the model and load
             curs = conn.cursor()
             for row in input_rows:
                 sql = "INSERT INTO RAW.{}".format(table_name) + \

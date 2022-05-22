e011bae858d3082ac1dec6e4ac63c5a1483cc15d
waylake <hwangdoyeonyu@gmail.com>
2022-05-22 23:01:06 +0900

    edit code for neoformat

--- utils/models/pre_set/main.py
+++ utils/models/pre_set/main.py
@@ -7,7 +7,7 @@ class Pre_set:
         self.info = info
 
     def _dir_check(self) -> None:
-        directories = self.info.base_info['dirs']
+        directories = self.info.base_info["dirs"]
 
         for directory in directories:
             if not os.path.exists(directory):
@@ -16,14 +16,14 @@ class Pre_set:
     def _selenium_options_config(self):
         from selenium import webdriver
 
-        pre_options = self.info.selenium_info['options']
-        pre_experi_options = self.info.selenium_info['prefs']
+        pre_options = self.info.selenium_info["options"]
+        pre_experi_options = self.info.selenium_info["prefs"]
 
         options: webdriver.ChromeOptions = webdriver.ChromeOptions()
         for pre_option in pre_options:
             options.add_argument(pre_option)
 
-        options.add_experimental_option('prefs', pre_experi_options)
+        options.add_experimental_option("prefs", pre_experi_options)
 
         return options
 
@@ -35,43 +35,41 @@ class Pre_set:
         import json
 
         model_json = {
-            'host': model.host,
-            'db_name': model.db_name,
-            'user': model.user,
-            'password': model.password,
-            'port': model.port,
+            "host": model.host,
+            "db_name": model.db_name,
+            "user": model.user,
+            "password": model.password,
+            "port": model.port,
         }
 
-        file_path = 'Data/model/database_models.json'
+        file_path = "Data/model/database_models.json"
         if not os.path.exists(file_path):
-            with open(file_path, 'w', encoding='utf-8') as create_file:
-                json_data = {'databases': {}}
+            with open(file_path, "w", encoding="utf-8") as create_file:
+                json_data = {"databases": {}}
                 json.dump(json_data, create_file, indent=4)
 
-        with open(file_path, 'r', encoding='utf-8') as json_file:
+        with open(file_path, "r", encoding="utf-8") as json_file:
             json_data = json.load(json_file)
-            json_data['databases'][model.db_name] = model_json
+            json_data["databases"][model.db_name] = model_json
 
-        with open(file_path, 'w', encoding='utf-8') as out_file:
+        with open(file_path, "w", encoding="utf-8") as out_file:
             json.dump(json_data, out_file, indent=4)
 
     def _get_conn(self, db_name):
         import json
         import pymysql
 
-        file_path = 'Data/model/database_models.json'
-        with open(file_path, 'r', encoding='utf-8') as json_file:
+        file_path = "Data/model/database_models.json"
+        with open(file_path, "r", encoding="utf-8") as json_file:
             json_data = json.load(json_file)
 
-        host = json_data[db_name]['host']
-        user = json_data[db_name]['user']
-        password = json_data[db_name]['password']
-        port = json_data[db_name]['port']
+        host = json_data[db_name]["host"]
+        user = json_data[db_name]["user"]
+        password = json_data[db_name]["password"]
+        port = json_data[db_name]["port"]
 
-        conn = pymysql.connect(host = host,
-                               db=db_name,
-                               user=user,
-                               password=password,
-                               port=port)
+        conn = pymysql.connect(
+            host=host, db=db_name, user=user, password=password, port=port
+        )
 
         return conn

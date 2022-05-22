9ee2db9d80cb6a6a6931c53eaab68dd3ad295d0a
waylake <hwangdoyeonyu@gmail.com>
2022-05-22 15:18:12 +0900

    edit code get conn

--- utils/utils.py
+++ utils/utils.py
@@ -19,17 +19,24 @@ class Utils:
         self.base_info = base.base_info
         self.etc_info = etc_info
         self.pre_set = Pre_set(info=base)
+        self.value: Dict = {}
         self._parameter_check()
 
         self.logger = CustomLogger().config()
         self.db_handler = DB_handler()
-        self.value: Dict = {}
 
     def _parameter_check(self):
         kwargs_info = base.kwargs_info
         inputed_param = list(self.kwargs.keys())
         defined_param = list(kwargs_info.keys())
         selected_params: List = set(inputed_param) & set(defined_param)
+
+        if 'db_conn' in selected_params:
+            func = kwargs_info['db_conn']['func']
+            _return_name = kwargs_info['db_conn']['_return']
+            exec(f"{_return_name} = self.pre_set.{func}(self.kwargs['db_conn'][1])")
+            self.value[_return_name] = eval(_return_name)
+            selected_params.remove('db_conn')
 
         for param in selected_params:
             if (kwargs_info[param]['type'] == 'func' and

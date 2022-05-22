62254931492c29abdbb733a3618a47abc4b8182f
waylake <hwangdoyeonyu@gmail.com>
2022-05-22 17:52:09 +0900

    edit function name

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ class Base_model:
         self._etc_info = etc_info
         self.utils = Utils(etc_info=self._etc_info, **kwargs)
 
-    def get_value(self):
+    def params_value(self):
         return self.utils.value

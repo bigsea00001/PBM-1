59cd3f6da9008e87a709b2172138501e5fe77460
waylake <hwangdoyeonyu@gmail.com>
2022-05-25 18:13:14 +0900

    test for hinting the *args's variable

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-from typing import Dict
-k: Dict = {'a': None}
+from typing import Tuple, overload
 
-exec(f'{list(k.keys())[0]} = 2', {'__builtins__' : None}, k)
-print(k)
+@overload
+def foo(arg1: int, arg2: int) -> Tuple[int, int]:
+    pass
+
+@overload
+def foo(arg: int) -> int:
+    pass
+
+def foo(*args):
+    try:
+        i, j = args
+        return j, i
+    except ValueError:
+        assert len(args) == 1
+        i = args[0]
+        return i
+

173c26939f60abab0a1b019a6000bc10e4ee3ba2
waylake <hwangdoyeonyu@gmail.com>
2022-05-25 17:41:57 +0900

    add func that parser the etc_info's value

--- utils/utils.py
+++ utils/utils.py
@@ -50,3 +50,18 @@ class Utils:
                     _return_name = kwargs_info[param]["_return"]
                     exec(f"{_return_name} = self.pre_set.{func}()")
                     self.value[_return_name] = eval(_return_name)
+
+    def _etc_info_check(self):
+        _info = self.etc_info
+        _return_data: Dict = {}
+        if isinstance(self.etc_info, Dict):
+            _info: Dict = _info
+            _keys = _info.keys()
+            if 'database' in _keys:
+                _db_model = DB_model(host=_info['database']['host'],
+                                     db_name=_info['database']['db_name'],
+                                     user=_info['database']['user'],
+                                     password=_info['database']['password'],
+                                     port=_info['database']['port'])
+                _conn = self.db_handler.get_conn(_db_model)
+                _return_data['db_conn'] = _conn

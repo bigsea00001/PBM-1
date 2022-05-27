39c1b97d48034d177b9ec9871a7620a14c912326
waylake <hwangdoyeonyu@gmail.com>
2022-05-27 19:39:48 +0900

    edit code

--- utils/info/_database.py
+++ utils/info/_database.py
@@ -1,21 +1,23 @@
-table_list = [
-    'table_name',
-    'table_name',
-    'table_name',
-]
+database = {
+    'table_list': [
+        'table_name',
+        'table_name',
+        'table_name',
+    ],
 
-table_info = {
-    'table_name': {
-        'scheme': [
-            'scheme_name_0',
-            'scheme_name_1',
-            'scheme_name_2',
-        ],
+    'table_info': {
+        'table_name': {
+            'scheme': [
+                'scheme_name_0',
+                'scheme_name_1',
+                'scheme_name_2',
+            ],
 
-        'to_scheme': {
-            'scheme_name_0': 'scheme_name_0',
-            'scheme_name_1': 'scheme_name_1',
-            'scheme_name_2': 'scheme_name_2',
+            'to_scheme': {
+                'scheme_name_0': 'scheme_name_0',
+                'scheme_name_1': 'scheme_name_1',
+                'scheme_name_2': 'scheme_name_2',
+            },
         },
-    },
+    }
 }

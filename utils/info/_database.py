3716de1bc301e34d26ed2c335174e78aad195151
waylake <hwangdoyeonyu@gmail.com>
2022-05-27 19:35:25 +0900

    edit code for database example

--- utils/info/_database.py
+++ utils/info/_database.py
@@ -7,11 +7,15 @@ table_list = [
 table_info = {
     'table_name': {
         'scheme': [
-            'scheme_name'
+            'scheme_name_0',
+            'scheme_name_1',
+            'scheme_name_2',
         ],
 
         'to_scheme': {
-            'scheme': 'scheme'
+            'scheme_name_0': 'scheme_name_0',
+            'scheme_name_1': 'scheme_name_1',
+            'scheme_name_2': 'scheme_name_2',
         },
     },
 }
